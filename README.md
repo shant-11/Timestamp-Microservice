983b00b46aaf7b71046912e62e4bd7557f9a51ae
Replit user <>
2023-07-05 04:38:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Timestamp Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+This is the my source code for the Timestamp Microservice project. Instructions for building  project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
--- index.js
+++ index.js
@@ -19,9 +19,31 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+     let dateData = new Date(req.params.date);
+  
+     if(dateData.toUTCString() === "Invalid Date"){
+        dateData = new Date(+req.params.date);
+       
+       if(dateData.toUTCString() === "Invalid Date"){
+       res.json({
+         error: "Invalid Date"
+       });
+       return;
+       }
+     }
+
+    res.json({
+      unix: dateData.getTime(),
+      utc: dateData.toUTCString()
+     });
+});
+
+app.get("/api", function( req, res) {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
 });
 
 
